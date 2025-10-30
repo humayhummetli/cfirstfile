83c2353d3266eb7950410c6c54d9dbfe62b0a081
humay <humay.hummatli@gmail.com>
2025-10-30 22:23:57 +0400

    Implement square matrix printing in square2.cpp
    Prints a square matrix with 1s on the diagonal and last column.
    input1
    5(Odd number)
    output1
    1 0 0 0 1
    0 1 0 1 0
    0 0 1 0 0
    0 1 0 1 0
    1 0 0 0 1

---
+++ square2.cpp
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+int main(){
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            if(j==i || j+i==n-1) printf("%d",1);
+            else printf("%d",0);
+        }
+        printf("\n");
+    }
+    return 0;
+}
