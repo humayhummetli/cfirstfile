d3e4c9b4246a6a11563df5e34c09f6b3ad245e1e
humay <humay.hummatli@gmail.com>
2025-10-30 22:32:36 +0400

    Add square4.cpp to print a square pattern
    input3
    5
    output3
    1 2 2 2 2
    0 1 2 2 2
    0 0 1 2 2
    0 0 0 1 2
    0 0 0 0 1

---
+++ square4.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            if(j<i) printf("%d",0);
+            else if(j==i) printf("%d",1);
+            else printf("%d",2);
+        }
+        printf("\n");
+    }
+    return 0;
+}
